ca4cba43b1abdf3a95c41fd0696657d7e0c58d4d
Timothy Roy <timothy.roy@protonmail.ch>
2022-11-15 20:14:31 -0600

    feat: use WalletModalButton and WalletMultiButton in AppBar

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,13 +1,21 @@
-import { FC } from 'react'
-import styles from '../styles/Home.module.css'
-import Image from 'next/image'
+import { FC } from "react";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import {
+  WalletMultiButton,
+  WalletModalButton,
+} from "@solana/wallet-adapter-react-ui";
+
+// WalletMultiButton handles connecting to _app.tsx ???
+// don't really believe it tbh
 
 export const AppBar: FC = () => {
-    return (
-        <div className={styles.AppHeader}>
-            <Image src="/solanaLogo.png" height={30} width={200} />
-            <span>Wallet-Adapter Example</span>
-            <button>Connect</button>
-        </div>
-    )
-}
+  return (
+    <div className={styles.AppHeader}>
+      <Image src="/solanaLogo.png" height={30} width={200} />
+      <span>Wallet-Adapter Example</span>
+      <WalletModalButton />
+      <WalletMultiButton />
+    </div>
+  );
+};
