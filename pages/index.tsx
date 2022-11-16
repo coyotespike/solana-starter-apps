ffb0f8a95c08c128c2fb46dd25d21c95cb10235d
Timothy Roy <timothy.roy@protonmail.ch>
2022-11-15 20:17:51 -0600

    feat: update index.tsx to use buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,28 @@
-import { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
-import { AppBar } from '../components/AppBar'
-import Head from 'next/head'
-import { PingButton } from '../components/PingButton'
+import { NextPage } from "next";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import { AppBar } from "../components/AppBar";
+import Head from "next/head";
+import { PingButton } from "../components/PingButton";
+import { AirDropButton } from "../components/AirDropButton";
 
 const Home: NextPage = (props) => {
+  const [balance, setBalance] = useState(0);
 
   return (
     <div className={styles.App}>
       <Head>
         <title>Wallet-Adapter Example</title>
-        <meta
-          name="description"
-          content="Wallet-Adapter Example"
-        />
+        <meta name="description" content="Wallet-Adapter Example" />
       </Head>
-        <AppBar />
-        <div className={styles.AppBody}>
-          <PingButton/>
-        </div>
+      <AppBar />
+      <div className={styles.AppBody}>
+        <p>Balance: {balance}</p>
+        <PingButton />
+        <AirDropButton setBalance={setBalance} />
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
