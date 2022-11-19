9c595eff44a49ca8dcb9aebcec854646fad1df60
Timothy Roy <timothy.roy@protonmail.ch>
2022-11-19 16:19:23 -0600

    feat: create FetchNft component using in-browser wallet

--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -1,10 +1,58 @@
-import { useConnection, useWallet } from "@solana/wallet-adapter-react"
-import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js"
-import { FC, useEffect, useState } from "react"
-import styles from "../styles/custom.module.css"
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Metaplex, walletAdapterIdentity } from "@metaplex-foundation/js";
+import { FC, useEffect, useState } from "react";
+import styles from "../styles/custom.module.css";
 
 export const FetchNft: FC = () => {
-  const fetchNfts = async () => {}
+  const [nftData, setNftData] = useState(null);
 
-  return <div></div>
-}
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  const metaplex = Metaplex.make(connection).use(walletAdapterIdentity(wallet));
+
+  // fetch nfts
+  const fetchNfts = async () => {
+    if (!wallet.connected) {
+      return;
+    }
+
+    // fetch NFTs for connected wallet
+    const nfts = await metaplex
+      .nfts()
+      .findAllByOwner({ owner: wallet.publicKey })
+      .run();
+
+    // fetch off chain metadata for each NFT
+    let nftData = [];
+    await Promise.all(
+      nfts.map(async (nft) => {
+        let fetchResult = await fetch(nft.uri);
+        let json = await fetchResult.json();
+        nftData.push(json);
+      })
+    );
+
+    // set state
+    setNftData(nftData);
+  };
+
+  // fetch nfts when connected wallet changes
+  useEffect(() => {
+    fetchNfts();
+  }, [wallet]);
+
+  return (
+    <div>
+      {nftData && (
+        <div className={styles.gridNFT}>
+          {nftData.map((nft) => (
+            <div key={nft.name}>
+              <ul>{nft.name}</ul>
+              <img src={nft.image} />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
